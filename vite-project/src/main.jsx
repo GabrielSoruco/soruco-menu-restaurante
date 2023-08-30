d98fbd52bd450d633b876d63d49e9acec052763d
GabrielSoruco <gabrielsorucogs@gmail.com>
2023-08-30 19:37:56 -0300

    Agregado CustomNavbar al main.jsx y eliminados los archivos css

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import CustomNavbar from './components/CustomNavbar.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <CustomNavbar/>
   </React.StrictMode>,
 )
