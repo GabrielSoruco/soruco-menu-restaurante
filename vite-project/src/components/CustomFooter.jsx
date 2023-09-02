0f1a3fdf03ef3f9a6c500274cbced6ade71e1193
GabrielSoruco <gabrielsorucogs@gmail.com>
2023-09-02 20:14:16 -0300

    AgregadoS los componentes navbar y footer, vinculados a main.jsx git status
    git status
    
    git status

---
+++ vite-project/src/components/CustomFooter.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+
+
+<footer className="pb-5 pt-5 bg-secondary">
+    <Container className="text-center">
+        <h5>Homeburger</h5>
+        <span>Todos los derechos reservados &copy;</span>
+    </Container>
+</footer>
+
+
+export default CustomFooter
